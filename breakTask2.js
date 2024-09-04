26a715002722fe35a7ab0681f165435ffdb10887
jishanprodhan12 <mdjishanprodhan132@gmail.com>
2024-09-04 04:58:19 -0500

    contine and break

--- breakTask2.js
+++ breakTask2.js
@@ -5,10 +5,21 @@ Write a while loop that adds numbers starting from 1, but stops (using break) as
 let sum = 0 , n = 100;
 for(let i = 1 ; i< n ; i++){
     sum+=i;
+
     if(sum >= 100){
         break;
     }
-   
     
     console.log(sum+" + "+i+" = "+sum);
+}
+console.log('using while loop ');
+let i=1 ,total =0;
+while(true){
+    total+=i;
+    if(total>=100){
+        break;
+    }
+    i++;
+    console.log(total+" + "+i+" = "+total);
+
 }
