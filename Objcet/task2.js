68184c0e22796cfaed02cc390484cfb4834c81e4
jishanprodhan12 <mdjishanprodhan132@gmail.com>
2024-09-08 12:05:02 -0500

    object task

---
+++ Objcet/task2.js
@@ -0,0 +1,19 @@
+/**
+ * For this object below add a property named passenger capacity with value 5
+ * and isRent = true 
+
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+};
+ */
+const car = { 
+    make : "Toyota",
+    model :"Corolla",
+    year : 2020
+};
+console.log(car);
+car["passenger capacity"] = 5 ;
+car.isRent = true;
+console.log(car);
---
+++ Objcet/task4.js
@@ -0,0 +1,20 @@
+/**
+ * Count the number of properties.
+
+Input:
+
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+ */
+ let student = {
+    name : 'Ariana Grande ',
+    age : 21 ,
+    city : 'Gaibandha ',
+    isStudent : true 
+ };
+ const keys =Object.keys(student);
+ console.log(keys);
