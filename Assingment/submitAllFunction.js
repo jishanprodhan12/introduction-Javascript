fa9caa8abdac802412c1dce5222fad729db8f3b4
jishanprodhan12 <mdjishanprodhan132@gmail.com>
2024-09-10 13:02:47 -0500

    task 3 done

--- Assingment/submitAllFunction.js
+++ Assingment/submitAllFunction.js
@@ -1,6 +1,6 @@
 function calculateTax(income, expenses) {
     if(income < 0 || expenses < 0 || income < expenses ){
-      return "Invalid Input"
+      return "Invalid Input" ;
     }
     return ( income - expenses ) * .20 ;
   }
@@ -15,4 +15,17 @@ function calculateTax(income, expenses) {
    const domainName = email.split('@').splice(1);
    const mgs = userName.concat("sent you an email from").concat(domainName);
    return mgs.join(' ') ;
+}
+
+
+function checkDigitsInName(name) {
+    if(typeof name !== 'string'){
+        return "Invalid Input" ;
+    }
+    for(let i = 0 ; i<name.length; i++){
+        if (name[i] >= '0' && name[i] <= '9') {
+            return true; 
+        }
+    }
+    return false; 
 }
